3810db579cb09548af3e08b4474ce4f1b1083b03
Zhengnan817 <144702539+Zhengnan817@users.noreply.github.com>
2023-11-30 13:18:46 -0500

    update main file

--- src/chip_analysis/__main__.py
+++ src/chip_analysis/__main__.py
@@ -37,45 +37,44 @@ def exploratory_data_analysis():
     my_file_path = "https://raw.githubusercontent.com/Zhengnan817/Project-3-Data-Reconstruction-and-Analysis/main/src/chip_analysis/data/chip_dataset.csv"
     cpu_table = DataProcess(my_file_path)
     df = cpu_table.view_data
-
     EDA_part = EDA(df)
-    EDA_part.vendor_distribution_sns()
+    while True:
+        EDA_part.vendor_distribution_sns()
 
-    EDA_part.vendor_distribution_plt()
+        EDA_part.vendor_distribution_plt()
 
-    EDA_part.type_vendor_sns()
+        EDA_part.type_vendor_sns()
 
-    EDA_part.type_vendor_plt()
+        EDA_part.type_vendor_plt()
 
-    EDA_part.process_size_sns()
+        EDA_part.process_size_sns()
 
-    EDA_part.process_size_plt()
+        EDA_part.process_size_plt()
 
-    EDA_part.process_size_vendor_sns()
+        EDA_part.process_size_vendor_sns()
 
-    EDA_part.process_size_vendor_plt()
+        EDA_part.process_size_vendor_plt()
 
-    EDA_part.TDP_distribution_sns()
+        EDA_part.TDP_distribution_sns()
 
-    EDA_part.TDP_distribution_plt()
+        EDA_part.TDP_distribution_plt()
 
 
 def inferences():
     my_file_path = "https://raw.githubusercontent.com/Zhengnan817/Project-3-Data-Reconstruction-and-Analysis/main/src/chip_analysis/data/chip_dataset.csv"
     cpu_table = DataProcess(my_file_path)
     df = cpu_table.view_data
-
     inferences_analysis = Inferences(df)
+    while True:
+        inferences_analysis.vendor_type_plt()
 
-    inferences_analysis.vendor_type_plt()
+        inferences_analysis.vendor_type_sns()
 
-    inferences_analysis.vendor_type_sns()
+        inferences_analysis.chip_attribute()
 
-    inferences_analysis.chip_attribute()
+        inferences_analysis.ave_freq_type_ven()
 
-    inferences_analysis.ave_freq_type_ven()
-
-    inferences_analysis.freq_and_TDP()
+        inferences_analysis.freq_and_TDP()
 
 
 main()
